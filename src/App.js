c7a27af8cafd28a6e108024771906fae332f2c1e
Regis <regis.silva.ads@gmail.com>
2021-06-21 13:32:46 -0300

    Primeiro commit tutoria react

---
+++ src/App.js
@@ -0,0 +1,37 @@
+// import logo from './logo.svg';
+// import React, { Component } from 'react';
+import './App.css';
+// import Greet from "./components/Greet";
+// import Welcome from "./components/Welcome";
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+// import Message from './components/message';
+// import Hello from './components/Hello';
+// import Counter from './components/Counter';
+
+function App() {
+  return (
+    <div className="App">
+      <ParentComponent />
+      {/* <FunctionClick /> */}
+      {/* <ClassClick /> */}
+      {/* <EventBind /> */}
+      {/*<Counter />*/}
+      {/*<Message />*/}
+      {/*<Greet name="Bruce" heroName="Batman">*/}
+      {/*<p>This is children props</p></Greet>*/}
+      {/*<Greet name="Clark" heroName="Superman"> </Greet>*/}
+      {/*<button>Action</button>*/}
+      {/* <Greet name="Diana" heroName="Wonder Woman"> </Greet> */}
+      {/* <Welcome name="Bruce" heroName="Batman" /> */}
+      {/* <Welcome name="Clark" heroName="Superman" /> */}
+      {/* <Welcome name="Diana" heroName="Wonder Woman" /> */}
+      {/*<Hello />*/}
+    </div>
+  );
+
+}
+
+export default App;
